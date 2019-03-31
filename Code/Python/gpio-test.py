b75d585fb5702584aa93aa75fff249d6ecf494df
Manolis Kiagias <sonic2000gr@gmail.com>
2019-03-31 21:21:02 +0300

    Add proof of concept test python script for Z80-RasPI communication

---
+++ Code/Python/gpio-test.py
@@ -0,0 +1,30 @@
+import pigpio
+import time
+
+def z80_write(gpio, level, tick):
+  global cb1
+  cb1.cancel()
+  data = pi.read_bank_1()
+  data = (data & 1044480) >> 12
+  print bin(data), chr(data)
+  cb1 = pi.callback(10, pigpio.FALLING_EDGE, z80_write)
+  return
+
+pi = pigpio.pi()
+
+# GPIO10 = Z80 Write (active low)
+pi.set_mode(10, pigpio.INPUT)
+
+# GPIO12 - 19 = Z80 Data bus (active high)
+for i in range(0,32):
+  pi.set_pull_up_down(i, pigpio.PUD_DOWN)
+
+for i in range(12,20):
+  pi.set_mode(i, pigpio.INPUT)
+  
+# Define callback for Z80 Write
+
+cb1 = pi.callback(10, pigpio.FALLING_EDGE, z80_write)
+while True:
+  pass
+    
